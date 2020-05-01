3c1201875af2b2e62f74730c9c697642606df347
AaronOBrien1453 <aatobrien@gmail.com>
2020-05-01 14:37:52 -0400

    number one not done

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Add validation!');
+let formIsValid = true;
+let inputsAll = document.querySelectorAll("input");
+let form = document.querySelector("#parking-form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  checkEmpty(); //we need to check each input to see if it's empty
+});
+
+//HELPER FUNCTIONS//
+function markValid() {
+  formIsValid = true;
+}
+function markInvalid() {
+  //   formIsValid = false;
+}
+
+//validation functions//
+function checkEmpty() {
+  for (let input of inputsAll) {
+    if (input.value === "") {
+      markInvalid();
+    }
+  }
+}
